790206a347fc49dbdd48750d5e513a90e45ce1a1
Jorge Covarrubias <artnhector@gmail.com>
2018-09-19 14:42:52 -0500

    Add onItemClick to generate NowPlayingActivity

--- app/src/main/java/com/example/android/musicappudacity/SongsActivity.java
+++ app/src/main/java/com/example/android/musicappudacity/SongsActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.musicappudacity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -22,6 +26,27 @@ public class SongsActivity extends AppCompatActivity {
         SongAdapter adapter = new SongAdapter(this, music);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
+
+        // Listener for when an item is clicked from the generated list
+        // This wasn't a copy but I learned it from watching
+        // https://www.youtube.com/watch?time_continue=491&v=D3OP14jZcxY
+        // Credit goes to Matthew Bailey
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                CompositionClass currentSong = music.get(i);
+                String songName = currentSong.getSong();
+                String artistName = currentSong.getArtist();
+                int albumImage = currentSong.getImageResourceId();
+                // This will start the activity when the item is clicked
+                Intent nowPlayingIntent = new Intent(SongsActivity.this, NowPlayingActivity.class);
+                nowPlayingIntent.putExtra("song", songName);
+                nowPlayingIntent.putExtra("artist", artistName);
+                nowPlayingIntent.putExtra("album", albumImage);
+                startActivity(nowPlayingIntent);
+
+            }
+        });
 
     }
 }
